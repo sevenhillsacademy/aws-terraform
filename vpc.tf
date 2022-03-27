2bb21bb9ea41616635cf7647fe53324849be9290
sevenhillsacademy <100567650+sevenhillsacademy@users.noreply.github.com>
2022-03-27 07:34:56 +0530

    updated vpc

--- vpc.tf
+++ vpc.tf
@@ -1,17 +1,9 @@
-# Configure the AWS Provider
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
+# Configure the AWS VPC
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "main"
   }
-}
-
-# Configure the AWS Provider
-
-provider "aws" {
-  region = "us-east-1"
-  access_key = "AKIAWDQ47ASP3JZDUS4Y"
-  secret_key = "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"
 }
